48044d71ed37aa026dab969b84869b6c602fe7e0
joe <doncrafttv@gmail.com>
2023-01-09 14:43:32 +0200

    kubernetes implementation

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const http = require('http');
+const url = require('url')
 const port = 80
 
 const requestListener = function (req, res) {
     res.writeHead(200);
+    let path = url.parse(req.url).pathname
+    if (path === "/what-is-love") {
+        res.end("baby don't hurt me")
+        return
+    }
+
     res.end('I love Dumplings! :)');
 }
 
-console.log("Starting server at port "+port)
+console.log("Starting server at port " + port)
 const server = http.createServer(requestListener);
 server.listen(port);
